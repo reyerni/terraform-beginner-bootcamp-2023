33b8ba67ba37c5da8154bb91357e285bb5f9e208
reyerni reyes <ernireyes24@gmail.com>
2023-11-07 03:21:56 +0000

    terraform cloud update

--- main.tf
+++ main.tf
@@ -1,19 +1,49 @@
 terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "terraform-beginner-bootcamp-20234"
+
+  #  workspaces {
+  #    name = "terra-house-24"
+  #  }
+  #}
+  cloud {
+    organization = "terraform-beginner-bootcamp-20234"
+    workspaces {
+      name = "terra-house-24"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.24.0"
+    }
   }
 }
 
+provider "aws" {
+}
 provider "random" {
   # Configuration options
 }
 
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  length   = 16
+  lower = true
+  upper = false
+  length   = 32
   special  = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
